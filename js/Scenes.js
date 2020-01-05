1574c57f18f77359b68184ce4441f8af1bbbd806
James Van Roose <32675940+jvanroose@users.noreply.github.com>
2020-01-05 13:19:18 +0000

    update

--- js/Scenes.js
+++ js/Scenes.js
@@ -5,6 +5,8 @@ class BaseScene extends Phaser.Scene {
     camera;
     exitLayer;
     score;
+    gems;
+    skulls;
 
     constructor(key) {
         super(key);
@@ -25,15 +27,30 @@ class BaseScene extends Phaser.Scene {
         this.map.createStaticLayer('platforms', [this.map.landscape, this.map.props], 0, 0);
         this.exitLayer = this.map.createStaticLayer('exit', [this.map.landscape, this.map.props], 0, 0);
 
+        //Create groups
+        this.gems = this.physics.add.staticGroup();
+        this.skulls = this.physics.add.group();
+
         //Create from object layer(s)
         this.map.getObjectLayer("objects").objects.forEach(function(object){
-            if(object.type === "playerSpawner"){
-                
-            }
-        })
+            object = this.retrieveCustomProperties(object);
 
-        //Create player
-        this.createPlayer();
+            if(object.type === "playerSpawner"){
+                //Create player
+                this.createPlayer(object.x, object.y);
+            }
+
+            if(object.type === "pickup"){
+                //Create gem
+                this.createGem(object);
+            }
+
+            if(object.type === "enemySpawner"){
+                //Create enemy
+                this.createEnemy(object);
+            }
+        }, this);
+
 
         //Create foreground layers
         this.map.createStaticLayer('foreground', [this.map.landscape, this.map.props], 0, 0);
@@ -71,10 +88,34 @@ class BaseScene extends Phaser.Scene {
 
     }
 
-    createPlayer() {
+    createPlayer(x, y) {
         //Add sprite to world
-        this.player = this.physics.add.sprite(16, 16, 'player', 1);
+        this.player = this.physics.add.sprite(x, y, 'player', 1);
         this.player.setCollideWorldBounds(true);
+    }
+
+    createGem(object) {
+        //Add sprite 
+        this.gems.create(object.x, object.y, object.sprite);
+    }
+
+    createEnemy(object) {
+        //Add sprite
+        //let skull = this.skulls.create(object.x, object.y);
+        let origin = {x: object.x, y: object.y+object.height};
+        let dest = {x: object.x+object.width, y: object.y+object.height};
+        let line = new Phaser.Curves.Line(origin, dest);
+        let skull = this.add.follower(line, origin.x, origin.y, 'skull');
+        this.physics.add.existing(skull);
+        this.skulls.add(skull);
+
+        skull.startFollow({
+            duration: 1000,
+            repeat: -1,
+            yoyo: true,
+            ease: 'Sine.easeInOut'
+        })
+
     }
 
     createCollision() {
@@ -82,9 +123,26 @@ class BaseScene extends Phaser.Scene {
         let collisionLayer = this.map.getLayer('platforms').tilemapLayer;
         collisionLayer.setCollisionBetween(0, 1000);
 
-        //Enable collision between player and "platforms" layer
+        //Enable collision between player/skulls and "platforms" layer
         this.physics.add.collider(this.player, collisionLayer);
+        this.physics.add.collider(this.skulls, collisionLayer);
 
+        //Enable overlap detection for player and gems/skulls
+        this.physics.add.overlap(this.player, this.gems, function(){console.log("winner")});
+        this.physics.add.overlap(this.player, this.skulls, function(){console.log("dead")});
+
+    }
+
+    retrieveCustomProperties(object) {
+        if(object.properties) { //Check if the object has custom properties
+            object.properties.forEach(function(element){ //Loop through each property
+                this[element.name] = element.value; //Create the property in the object
+            }, object); //Assign the word "this" to be the object
+
+            delete object.properties; //Delete the custom properties array from the object
+        }
+
+        return object; //Return the new object w/ custom properties
     }
 }
 class SceneA extends BaseScene {
@@ -95,6 +153,8 @@ class SceneA extends BaseScene {
         //Load assets
         this.load.image('landscape-image', '../assets/landscape-tileset.png');
         this.load.image('props-image', '../assets/props-tileset.png');
+        this.load.image('gem', '../assets/gem.png');
+        this.load.image('skull', '../assets/skull.png');
         this.load.spritesheet('player', '../assets/player.png', {
             frameWidth: 24,
             frameHeight: 24
@@ -112,6 +172,7 @@ class SceneA extends BaseScene {
     update(){
         super.update();
         let tile = this.exitLayer.getTileAtWorldXY(this.player.x, this.player.y);
+        
         if (tile) {
             switch (tile.index) {
                 case 200:
@@ -139,6 +200,7 @@ class SceneB extends BaseScene {
         //Load assets
         this.load.image('landscape-image', '../assets/landscape-tileset.png');
         this.load.image('props-image', '../assets/props-tileset.png');
+        this.load.image('gem', '../assets/gem.png');
         this.load.spritesheet('player', '../assets/player.png', {
             frameWidth: 24,
             frameHeight: 24
